ce389166f17687f992cce93642da9eb00997ac0f
Anton K <akarpenko83@gmail.com>
2023-03-20 16:59:53 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox"
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery")
+
+galleryContainer.addEventListener('click', onPictureClick)
+
+galleryContainer.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({description, original, preview}) => {
+        return `
+        <div class="gallery">
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+            </a> 
+        </div>
+        `;
+    })
+    .join("");
+};
+ 
+function onPictureClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return
+    }
+         handleModalWindow()
+};
+function handleModalWindow() {
+    let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+  };
