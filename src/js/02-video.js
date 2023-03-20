23a216fb1dfd39039edd5701368e57f3065aca9f
Anton K <akarpenko83@gmail.com>
2023-03-20 17:49:45 +0200

    task 2 started

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from "@vimeo/player";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+// iframe.addEventListener('timeupdate', onTimeUpdate);
+
+
+player.on('play', function() {
+console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+player.on('timeupdate', function ({ seconds }) {
+    
+});
