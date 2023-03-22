f9f27e5954cb367625d9b55bc67f78cbf793ed3d
Anton K <akarpenko83@gmail.com>
2023-03-22 13:10:31 +0200

    task-3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,60 @@
 const throttle = require('lodash.throttle');
 
+let formData = {
+    email:"",
+    message:"",
+}
+const STORAGE_KEY = "feedback-form-state";
+
 const refs = {
     form: document.querySelector(".feedback-form"),
-    input: document.querySelector('input'),
-    textarea: document.querySelector('textarea'),
-
+    email: document.querySelector(".feedback-form input"),
+    textarea: document.querySelector(".feedback-form textarea"),
 }
-console.log(refs.form);
-console.log(refs.input);
-console.log(refs.textarea);
 
-// console.log(localStorage.getItem("feedback-form-state"));
 refs.form.addEventListener("submit", onFormSubmit);
-refs.input.addEventListener("input", throttle(saveToLocalStorage, 500))
-refs.textarea.addEventListener("input", throttle(saveToLocalStorage, 500))
+refs.form.addEventListener("input", throttle(saveToLocalStorage, 500))
 
-function saveToLocalStorage(event) {
-    const storageData = {};
-    const userData = event.target.value;
+onload = () => {
+    let storedData = localStorage.getItem(STORAGE_KEY);
+    if (storedData) {
+        restoreFeedback(storedData)
+    }
 
-    storageData.email = userData;
-    localStorage.setItem("feedback-form-state", JSON.stringify(storageData));
 };
+
+function saveToLocalStorage({target}) {
+    const { value } = target;
+    target.setAttribute("value", value);
+    dataFill();
+    updateLocalStorage();
+};
+
+function dataFill() {
+    formData.email = refs.form.elements.email.getAttribute("value");
+    formData.message = refs.form.elements.message.getAttribute("value");
+}
+function updateLocalStorage() {
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function restoreFeedback(storedData) {
+    const restoredData = JSON.parse(storedData);
+    refs.form.elements.email.setAttribute("value", restoredData.email);
+    refs.form.elements.message.textContent = restoredData.message;
+}
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    interfaceCleaning();
+    console.dir(formData);
+};
+
+function interfaceCleaning() {
+    formData.email = "";
+    formData.message = "";
+    refs.form.elements.email.setAttribute("value", "");
+    refs.form.elements.message.textContent = "";
+ }
+
